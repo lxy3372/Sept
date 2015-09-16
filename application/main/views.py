3525d1d415e711921d628a1d9853a300125495cc
Riky <me@rikyliu.com>
2015-09-16 18:38:49 +0800

    add template

---
+++ application/main/views.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding=utf-8 -*-
+
+__author__ = 'Ricky'
+
+from flask import render_template
+from . import main
+
+
+@main.route('/')
+def index():
+    return "hello world"
+
+@main.route('/about')
+@main.route('/about/<anything>')
+def templates(anything=None):
+    return render_template('about.html', option=anything)
---
+++ run.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#-*- coding=utf-8 -*-
+__author__ = 'Riky'
+
+from application import app
+
+app.run()
+
+
