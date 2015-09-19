c912b524392796f967d2581d3c4845e11bcf8cdd
Riky <me@rikyliu.com>
2015-09-19 18:13:00 +0800

    add Bootstrap

--- application/__init__.py
+++ application/__init__.py
@@ -4,8 +4,14 @@
 __author__ = 'Riky'
 
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from main import main as main_blueprint
 
-app = Flask(__name__)
-app.register_blueprint(main_blueprint)
+
+def create_app(conifg = None):
+    app = Flask(__name__)
+    app.register_blueprint(main_blueprint)
+    Bootstrap(app)
+
+    return app
 
--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
-#-*- coding=utf-8 -*-
+# -*-  coding=utf-8 -*-
 __author__ = 'Riky'
 
-from application import app
+from application import create_app
 
+app = create_app()
 app.run()
 
 
