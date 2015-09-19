51d378affdf7669585926b454b623f1229dbb005
Riky <me@rikyliu.com>
2015-09-19 18:13:00 +0800

    add Bootstrap

--- application/main/views.py
+++ application/main/views.py
@@ -6,7 +6,6 @@ __author__ = 'Ricky'
 from flask import render_template
 from . import main
 
-
 @main.route('/')
 def index():
     return "hello world"
@@ -15,3 +14,8 @@ def index():
 @main.route('/about/<anything>')
 def templates(anything=None):
     return render_template('about.html', option=anything)
+
+
+@main.errorhandler(404)
+def page_not_found(e):
+    pass
