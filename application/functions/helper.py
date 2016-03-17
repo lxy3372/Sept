3901bffb549efb4bb0fd149e3093e931d29c65bc
Riky <me@rikyliu.com>
2016-03-17 09:39:09 +0800

    增加后台面板

--- application/functions/helper.py
+++ application/functions/helper.py
@@ -2,7 +2,7 @@
 # -*- coding=utf-8 -*-
 
 from functools import wraps
-from flask import redirect, session, current_app, flash
+from flask import redirect, session, current_app, flash, url_for, request, render_template
 
 __author__ = 'Riky'
 
@@ -12,6 +12,45 @@ def login_required(f):
     def decorated_function(*args, **kwargs):
         if not session.get(current_app.config.get('SESSION_KEY')):
             flash(u'没有登陆')
-            return redirect('/login')
+            return redirect(url_for('main.login_page'))
         return f(*args, **kwargs)
+
     return decorated_function
+
+
+def templated(template=None):
+    """
+    模板装饰器
+    :param template 模板路径
+    """
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            template_name = template
+            if template_name is None:
+                template_name = request.endpoint.replace('.', '/') + '.html'
+            ctx = f(*args, **kwargs)
+            if ctx is None:
+                ctx = {}
+            elif not isinstance(ctx, dict):
+                return ctx
+            return render_template(template_name, **ctx)
+
+        return decorated_function
+
+    return decorator
+
+
+def get_title_by_func(func_name):
+    """
+    返回页面标题
+    :param func_name: 方法名
+    :return: unicode str
+    """
+    func_title_dict = {
+        'panel': u"控制面板",
+        'login_page': u"登陆",
+        'index': u"首页",
+    }
+    title = func_title_dict[func_name]
+    return title if title else u"标题"
