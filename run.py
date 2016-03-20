37bcf4462ef16063c19e5eff8e8b93a87b348087
Riky <me@rikyliu.com>
2016-03-21 00:13:32 +0800

    新增commit ，增加model

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ __author__ = 'Riky'
 from application import create_app
 from datetime import *
 from flask import session
+from application.model.db import db
 
 app = create_app('dev')
 
@@ -14,6 +15,15 @@ def reverse_filter(f):
     date = now.strftime('%p')
     name = session.get(app.config.get('SESSION_KEY'))['nickname']
     return (u"上午好" if date == 'AM' else u"下午好") +u"，"+name
+
+@app.teardown_request
+def teardown_request(exception):
+    if exception:
+        db.session.rollback()
+        db.session.remove()
+    else:
+        db.session.commit()
+    db.session.remove()
 
 app.run()
 
